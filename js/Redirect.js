23db05b788947f9b849ac831936706493c1f8195
MoDerPo <90388353+Moritz-Tim-Widmann@users.noreply.github.com>
2022-01-10 08:19:43 +0100

    fixed mistake that resulted in suffix == undefined

--- js/Redirect.js
+++ js/Redirect.js
@@ -1,6 +1,8 @@
 function redirect(suffix){
     if(suffix){
         suffix = '.'+suffix
+    } else{
+        suffix = ''
     }
     url = "Klassen/"+document.forms["classSelection"]["classes"].value+suffix
     window.location.href = url;
