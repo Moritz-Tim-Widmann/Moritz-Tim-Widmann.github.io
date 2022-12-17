7b54ee9394e4d112b5583c1d77c2b980ed8e0d43
MoDerPo <90388353+moritz-t-w@users.noreply.github.com>
2022-12-17 02:41:58 +0100

    formatting

--- js/ClassList.js
+++ js/ClassList.js
@@ -1,52 +1,52 @@
 let options = []
 const CLASSES_PER_YEAR = 2
-const CLASSES = "abcdefghijklmnopqrstuvwxyz".substring(0,CLASSES_PER_YEAR).split("")
+const CLASSES = "abcdefghijklmnopqrstuvwxyz".substring(0, CLASSES_PER_YEAR).split("")
 
 /** returns an array of option objects */
 function getOptions(relativePrefix, absolutePrefix) {
-    let options = []
-    
+	let options = []
 
-    CLASSES.forEach(suffix => {
-        options.push({
-            relative : relativePrefix + suffix,
-            absolute : absolutePrefix + suffix
-        })
-    })
-    return options
+
+	CLASSES.forEach(suffix => {
+		options.push({
+			relative: relativePrefix + suffix,
+			absolute: absolutePrefix + suffix
+		})
+	})
+	return options
 }
 
 function getSchoolYear(date) {
-    year = date.getFullYear()
-    month = date.getMonth()+1
+	year = date.getFullYear()
+	month = date.getMonth() + 1
 
-    if(month < 8){
-        year--
-    }
-    return year
+	if (month < 8) {
+		year--
+	}
+	return year
 }
 
 for (let j = 1; j <= 3; j++) {
-    
-    /** the year we are in now */
-    let thisYear = getSchoolYear(new Date())
-    /** the last 2 digits of the year we are in now */
-    let thisYearShort = parseInt(thisYear.toString().substring(-2))
-    /** the last 2 digits of the year I_a got into the ims*/
-    let absoluteYear = thisYearShort - j + 1
 
-    /** the first two letters for the relative class name */
-    let relativePrefix = "I"+j
-    /** the last three letters for the absolute class name */
-    let absolutePrefix = "I"+absoluteYear
-    
-    getOptions(relativePrefix, absolutePrefix).forEach(option => { //for each class of that year
-        options.push(option)
-    })
+	/** the year we are in now */
+	let thisYear = getSchoolYear(new Date())
+	/** the last 2 digits of the year we are in now */
+	let thisYearShort = parseInt(thisYear.toString().substring(-2))
+	/** the last 2 digits of the year I_a got into the ims*/
+	let absoluteYear = thisYearShort - j + 1
+
+	/** the first two letters for the relative class name */
+	let relativePrefix = "I" + j
+	/** the last three letters for the absolute class name */
+	let absolutePrefix = "I" + absoluteYear
+
+	getOptions(relativePrefix, absolutePrefix).forEach(option => { //for each class of that year
+		options.push(option)
+	})
 }
-options.forEach(option =>{
-    let classes= document.getElementById("classes")
-    classes.appendChild(
-        new Option(option.relative+" / "+option.absolute, option.absolute)
-    )
+options.forEach(option => {
+	let classes = document.getElementById("classes")
+	classes.appendChild(
+		new Option(option.relative + " / " + option.absolute, option.absolute)
+	)
 })
